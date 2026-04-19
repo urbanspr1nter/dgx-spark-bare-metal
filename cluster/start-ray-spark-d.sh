1182c2a5b9e637d9fae862ce5f1f459b245cc9e6
Roger Ngo <urbanspr1nter@gmail.com>
2026-04-18 21:22:17 -0700

    Spark c and d

---
+++ cluster/start-ray-spark-d.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export VLLM_IFACE=enp1s0f0np0
+export NCCL_SOCKET_IFNAME=$VLLM_IFACE
+export GLOO_SOCKET_IFNAME=$VLLM_IFACE
+export NCCL_IB_HCA=rocep1s0f0,roceP2p1s0f0
+export NCCL_IB_DISABLE=0
+export VLLM_HOST_IP=169.254.124.67
+export RAY_memory_usage_threshold=0.99
+
+ray stop -f || true
+ray start --address=169.254.195.229:6379
