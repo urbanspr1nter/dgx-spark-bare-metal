2c8a169ec8f3f9fe28b8f31d685a84b08f3c78ec
Roger Ngo <urbanspr1nter@gmail.com>
2026-05-23 04:59:22 -0700

    Remove disabling of the cutlass fp8 kernels

--- cluster/start-ray-spark-a.sh
+++ cluster/start-ray-spark-a.sh
@@ -7,7 +7,6 @@ export NCCL_IB_HCA=rocep1s0f0
 export NCCL_IB_DISABLE=0
 export VLLM_HOST_IP=169.254.195.229
 export RAY_memory_usage_threshold=0.99
-export VLLM_DISABLED_KERNELS=CutlassFp8BlockScaledMMKernel
 
 
 ray stop -f || true
