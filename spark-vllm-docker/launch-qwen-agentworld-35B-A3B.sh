78afc42b12fab3358d57d7eac9e05749f1130c63
Roger Ngo <urbanspr1nter@gmail.com>
2026-06-28 07:22:49 -0700

    AgentWorld

---
+++ spark-vllm-docker/launch-qwen-agentworld-35B-A3B.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+HOST_1=169.254.122.182
+HOST_2=169.254.114.56
+PORT=8000
+CONTEXT_LENGTH=262144
+TP=2
+
+BASE_PATH=/home/rngo/code/spark-vllm-docker
+
+"$BASE_PATH/launch-cluster.sh" -n $HOST_1,$HOST_2 exec vllm serve \
+    --model Qwen/Qwen-AgentWorld-35B-A3B \
+    --host 0.0.0.0 \
+    --port $PORT \
+    --tensor-parallel-size $TP \
+    --max-model-len $CONTEXT_LENGTH \
+    --gpu-memory-utilization 0.9 \
+    --enable-auto-tool-choice \
+    --trust-remote-code \
+    --tool-call-parser qwen3_coder \
+    --reasoning-parser qwen3 \
+    --enable-prefix-caching \
+    --distributed-executor-backend ray
